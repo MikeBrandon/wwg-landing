6b7a84866ec58a310b51cf55f0cb20133905b02e
Mike Brandon <mikhaibrandon@gmail.com>
2025-01-29 12:21:00 +0300

    complete awards

--- src/components/AwardsCarousel.tsx
+++ src/components/AwardsCarousel.tsx
@@ -1,71 +1,75 @@
 import Image from "next/image";
 import { FaTrophy } from "react-icons/fa6";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const AwardsCarousel = () => {
     const awards = [
         {
-            title: "Tournament Champions",
-            description: "Best performing team of 2023",
-            winnerImage: "/youtube_2024.jpg", 
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Youtube Content Creator of the Year",
+            winnerImage: "/lulu.jpg", 
+            winnerUsername: "czyLulu",
+            yearWon: "2024",
         },
         {
-            title: "Community Choice",
-            description: "Most helpful community member",
-            winnerImage: "/youtube_2024.jpg",
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Twitch Content Creator of the Year",
+            winnerImage: "/someothergirl.jpg",
+            winnerUsername: "_someothergirl",
+            yearWon: "2024",
         },
         {
-            title: "Content Creator", 
-            description: "Best gaming content creator",
-            winnerImage: "/youtube_2024.jpg",
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "TikTok Content Creator of the Year",
+            winnerImage: "/icebaby.jpg",
+            winnerUsername: "icebaby484",
+            yearWon: "2024",
         },
         {
-            title: "Tournament Champions",
-            description: "Best performing team of 2023",
-            winnerImage: "/youtube_2024.jpg", 
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Best Technical Content Creator of the Year",
+            winnerImage: "/bloody.jpg",
+            winnerUsername: "BloodyActor",
+            yearWon: "2024",
         },
         {
-            title: "Community Choice",
-            description: "Most helpful community member",
-            winnerImage: "/youtube_2024.jpg",
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "PC Content Creator of the Year",
+            winnerImage: "/cecily.jpg",
+            winnerUsername: "cecily960",
+            yearWon: "2024",
         },
         {
-            title: "Content Creator", 
-            description: "Best gaming content creator",
-            winnerImage: "/youtube_2024.jpg",
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Mobile Content Creator of the Year",
+            winnerImage: "/ptk.jpg",
+            winnerUsername: "PTKAfrica",
+            yearWon: "2024",
         },
         {
-            title: "Tournament Champions",
-            description: "Best performing team of 2023",
-            winnerImage: "/youtube_2024.jpg", 
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Male Content Creator of the Year",
+            winnerImage: "/mamba.jpg",
+            winnerUsername: "mambaJB",
+            yearWon: "2024",
         },
         {
-            title: "Community Choice",
-            description: "Most helpful community member",
-            winnerImage: "/youtube_2024.jpg",
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Female Content Creator of the Year",
+            winnerImage: "/mich.jpg",
+            winnerUsername: "MICHPUBG",
+            yearWon: "2024",
         },
         {
-            title: "Content Creator", 
-            description: "Best gaming content creator",
-            winnerImage: "/youtube_2024.jpg",
-            winnerUsername: "username",
-            yearWon: "2023",
+            title: "Best Content Creator of the Year",
+            winnerImage: "/ptk.jpg",
+            winnerUsername: "PTKAfrica",
+            yearWon: "2024",
+        },
+        {
+            title: "Console Content Creator of the Year",
+            winnerImage: "/angel.jpg",
+            winnerUsername: "AngxlWolfie",
+            yearWon: "2024",
+        },
+        {
+            title: "Esports Caster of the Year",
+            winnerImage: "/eugene.jpg",
+            winnerUsername: "Eugene Abuderby",
+            yearWon: "2024",
         },
     ];
 
@@ -89,17 +93,17 @@ const AwardsCarousel = () => {
                 {visibleAwards.map((award, index) => (
                     <div
                         key={`${currentPage}-${index}`}
-                        className="bg-primary/100 aspect-square rounded-lg border border-secondary overflow-hidden transform transition-all duration-500 ease-in-out"
+                        className="bg-primary/100 aspect-square border border-secondary overflow-hidden transform transition-all duration-500 ease-in-out flex"
                     >
-                        <div className="relative h-3/5 w-full">
+                        <div className="relative h-full min-w-max">
                             <Image 
                                 src={award.winnerImage || "/placeholder-winner.jpg"}
                                 alt={`${award.title} Winner`}
                                 className="object-cover w-full h-full transition-opacity duration-500 ease-in-out"
-                                width={1920}
-                                height={1080}
+                                width={331}
+                                height={586}
                             />
-                            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-secondary/90 to-transparent h-16" />
+                            <div className="absolute bottom-0 left-0 right-0 h-16" />
                         </div>
                         <div className="p-4">
                             <FaTrophy className="text-white text-2xl mb-2" />
@@ -120,6 +124,14 @@ const AwardsCarousel = () => {
                         onClick={() => setCurrentPage(i)}
                     />
                 ))}
+            </div>
+            <div className="flex justify-center">
+                <Link
+                href="/awards"
+                className="inline-flex items-center bg-secondary text-white px-6 py-3 rounded-lg font-semibold hover:bg-opacity-90 transition-all mt-4"
+                >
+                    View More
+                </Link>
             </div>
         </div>
     );
