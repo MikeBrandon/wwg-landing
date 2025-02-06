d4b42ef6e66df70f117bec34620af8b11949f827
Mike Brandon <bkb.braock@gmail.com>
2025-02-06 13:26:27 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import { motion, AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 
 const font = Montserrat({
   subsets: ["latin"],
